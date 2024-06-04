38408ceb1db82d8bd74affb9a1db7e7809bd84f1
Sahil100111 <160855019+Sahil100111@users.noreply.github.com>
2024-06-04 23:50:46 +0500

    Add files via upload

---
+++ task29.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$gm="Good Morning";
+  if (timezone_name_get(karachi)>12) {
+    echo($gm);
+
+  } else  {
+    echo("Good Afternoon");
+  }
+  ?>
+</body>
+</html>
